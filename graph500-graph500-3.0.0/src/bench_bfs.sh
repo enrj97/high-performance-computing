4215f816a97b0b65b2759bac4eae3a5563b4c4f0
Dominik Schreiber <e1326110@student.tuwien.ac.at>
2021-02-01 15:15:05 +0100

    added results for non power of two tests

---
+++ graph500-graph500-3.0.0/src/bench_bfs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+srun -p q_student -N 32 --ntasks-per-node=24 -o results/graph500_BFS_32x24_sf17.txt --time=05:00 ./graph500_reference_bfs 17
+srun -p q_student -N 32 --ntasks-per-node=24 -o results/graph500_BFS_32x24_sf20.txt --time=05:00 ./graph500_reference_bfs 20
+srun -p q_student -N 32 --ntasks-per-node=24 -o results/graph500_BFS_32x24_sf23.txt --time=05:00 ./graph500_reference_bfs 23
+srun -p q_student -N 32 --ntasks-per-node=24 -o results/graph500_BFS_32x24_sf26.txt --time=05:00 ./graph500_reference_bfs 26
+
+srun -p q_student -N 32 --ntasks-per-node=16 -o results/graph500_BFS_32x16_sf17.txt --time=05:00 ./graph500_reference_bfs 17
+srun -p q_student -N 32 --ntasks-per-node=16 -o results/graph500_BFS_32x16_sf20.txt --time=05:00 ./graph500_reference_bfs 20
+srun -p q_student -N 32 --ntasks-per-node=16 -o results/graph500_BFS_32x16_sf23.txt --time=05:00 ./graph500_reference_bfs 23
+srun -p q_student -N 32 --ntasks-per-node=16 -o results/graph500_BFS_32x16_sf26.txt --time=05:00 ./graph500_reference_bfs 26
+
+srun -p q_student -N 32 --ntasks-per-node=12 -o results/graph500_BFS_32x12_sf17.txt --time=05:00 ./graph500_reference_bfs 17
+srun -p q_student -N 32 --ntasks-per-node=12 -o results/graph500_BFS_32x12_sf20.txt --time=05:00 ./graph500_reference_bfs 20
+srun -p q_student -N 32 --ntasks-per-node=12 -o results/graph500_BFS_32x12_sf23.txt --time=05:00 ./graph500_reference_bfs 23
+srun -p q_student -N 32 --ntasks-per-node=12 -o results/graph500_BFS_32x12_sf26.txt --time=05:00 ./graph500_reference_bfs 26
+
+srun -p q_student -N 32 --ntasks-per-node=8 -o results/graph500_BFS_32x8_sf17.txt --time=05:00 ./graph500_reference_bfs 17
+srun -p q_student -N 32 --ntasks-per-node=8 -o results/graph500_BFS_32x8_sf20.txt --time=05:00 ./graph500_reference_bfs 20
+srun -p q_student -N 32 --ntasks-per-node=8 -o results/graph500_BFS_32x8_sf23.txt --time=05:00 ./graph500_reference_bfs 23
+srun -p q_student -N 32 --ntasks-per-node=8 -o results/graph500_BFS_32x8_sf26.txt --time=05:00 ./graph500_reference_bfs 26
+
+srun -p q_student -N 32 --ntasks-per-node=6 -o results/graph500_BFS_32x6_sf17.txt --time=05:00 ./graph500_reference_bfs 17
+srun -p q_student -N 32 --ntasks-per-node=6 -o results/graph500_BFS_32x6_sf20.txt --time=05:00 ./graph500_reference_bfs 20
+srun -p q_student -N 32 --ntasks-per-node=6 -o results/graph500_BFS_32x6_sf23.txt --time=05:00 ./graph500_reference_bfs 23
+srun -p q_student -N 32 --ntasks-per-node=6 -o results/graph500_BFS_32x6_sf26.txt --time=05:00 ./graph500_reference_bfs 26
+
+srun -p q_student -N 32 --ntasks-per-node=4 -o results/graph500_BFS_32x4_sf17.txt --time=05:00 ./graph500_reference_bfs 17
+srun -p q_student -N 32 --ntasks-per-node=4 -o results/graph500_BFS_32x4_sf20.txt --time=05:00 ./graph500_reference_bfs 20
+srun -p q_student -N 32 --ntasks-per-node=4 -o results/graph500_BFS_32x4_sf23.txt --time=05:00 ./graph500_reference_bfs 23
+srun -p q_student -N 32 --ntasks-per-node=4 -o results/graph500_BFS_32x4_sf26.txt --time=05:00 ./graph500_reference_bfs 26
